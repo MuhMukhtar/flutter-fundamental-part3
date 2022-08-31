c857e0e148b6a5974977a0618976ace84ed29902
MuhMukhtar <mukhtar55217@gmail.com>
2022-09-01 00:21:17 +0800

    praktikum 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_fundamental_3/basic_widgets/forms.dart';
-import 'package:flutter_fundamental_3/basic_widgets/image_widget.dart';
+import 'basic_widgets/image_widget.dart';
+import 'basic_widgets/forms.dart';
+import 'basic_widgets/input_fields.dart';
 
 void main() {
   runApp(const MyApp());
@@ -51,6 +53,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  final TextEditingController _controller = TextEditingController.fromValue(
+      const TextEditingValue(text: "isi angka saja"));
 
   void _incrementCounter() {
     setState(() {
@@ -78,25 +82,39 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: GestureDetector(
-          // onTap: _incrementCounter,
-          // onDoubleTap: _incrementCounter,
-          onLongPress: _incrementCounter,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-              'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              const FormContoh(),
-            ],)
-        ),
+            onTap: _incrementCounter,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                Form(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      VerificationCodeFormField(controller: _controller),
+                      Builder(
+                        builder: (BuildContext subContext) => ElevatedButton(
+                          onPressed: () {
+                            final valid = Form.of(subContext)?.validate();
+                            if (kDebugMode) {
+                              print("valid: $valid");
+                            }
+                          },
+                          child: const Text("validate"),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
